e5ca270e4d567fffd7b091b3b1dcc3fa1da10368
HAO Ngo Van (VTI.D8) <hao.ngovan@vti.com.vn>
2024-03-07 13:43:28 +0700

    Create project

---
+++ src/test/java/hunre/it/document/DocumentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package hunre.it.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
